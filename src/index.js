0782c3828f9db2dbd36247c1f8dc8e7c05798e94
Aayush-Pandey05 <aayushpandeydsce23@gmail.com>
2026-04-11 16:54:09 +0530

    Reapply "implement matches post and get routes"
    This reverts commit b9972d276f9d54b5f98d0ef30b6429386da48cb8.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { matchRouter } from "./routes/matches.js";
 
 const app = express();
 const PORT = 8000;
@@ -6,9 +7,11 @@ const PORT = 8000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-	res.status(200).json({ message: "Express server is running." });
+  res.status(200).json({ message: "Express server is running." });
 });
 
+app.use("/matches", matchRouter);
+
 app.listen(PORT, () => {
-	console.log(`Server started at http://localhost:${PORT}`);
+  console.log(`Server started at http://localhost:${PORT}`);
 });
